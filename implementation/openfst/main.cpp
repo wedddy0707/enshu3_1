8646a13b4c6889242034344bbd6c7c78c77df1e6
wedddy0707 <ryoryo.ueda@gmail.com>
2020-04-24 18:26:42 +0900

    作り直した

---
+++ implementation/openfst/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <set>
+#include <map>
+#include <string>
+//#include "inc/dictionaryFst.h"
+//#include "inc/inputFst.h"
+#include "inc/symbol.h"
+#include "inc/myFst.h"
+
+int main() {
+  std::set<wchar_t>             characters      ;
+  std::set<std::wstring>        words           ;
+  std::map<std::wstring,double> cost_of_word    ;
+  std::map<wchar_t,int>         id_of_character ;
+  std::map<std::wstring,int>    id_of_word      ;
+
+  symbol::make (
+      &characters,
+      &words,
+      &cost_of_word,
+      &id_of_character,
+      &id_of_word
+      );
+  myFst::make (
+      id_of_character,
+      id_of_word,
+      cost_of_word
+      );
+
+
+  return 0;
+}
+
