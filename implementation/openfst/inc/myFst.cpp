cd9771f5d3d2c63169a51d39c3a0529bb520184b
wedddy0707 <ryoryo.ueda@gmail.com>
2020-04-26 14:35:08 +0900

    hoge

--- implementation/openfst/inc/myFst.cpp
+++ implementation/openfst/inc/myFst.cpp
@@ -24,7 +24,12 @@ namespace myFst {
     t.SetStart(q0);
     int q1 = t.AddState();
     t.SetFinal(q1);
-  
+
+    // Unknown Character が来たときの振る舞い
+    int q2 = t.AddState();
+    t.AddArc(q0,fst::StdArc(symbol::ID_OF_UNK,symbol::ID_OF_EPS,0.0,q2));
+    t.AddArc(q2,fst::StdArc(symbol::ID_OF_EPS,symbol::ID_OF_UNK,0.0,q1));
+
     for(auto iter : id_of_word) {
       // iter.first  : 語彙
       // iter.second : その語彙の ID
@@ -61,7 +66,7 @@ namespace myFst {
     FILE* fp = fopen_with_errmsg("./data/sample_input.txt","r");
 
     fgetws(input_string,100,fp);
-    //fputws(L"input_string : ",stdout);
+    fputws(L"input_string : ",stdout);
     fputws(input_string,stdout);
   
     int q0 = t.AddState();
@@ -69,8 +74,20 @@ namespace myFst {
   
     int prev_q = q0;
     for(auto c : std::wstring(input_string)) {
+      if(c==L'\n') {
+        continue;
+      }
+
       int q = t.AddState();
-      t.AddArc(prev_q, fst::StdArc(id_of_character[c],id_of_character[c],0.0,q));
+      fst::StdArc a;
+
+      if (id_of_character.find(c)==id_of_character.end()) { // Unknown Character
+        a = fst::StdArc(symbol::ID_OF_UNK,symbol::ID_OF_UNK,0.0,q);
+      } else {
+        a = fst::StdArc(id_of_character[c],id_of_character[c],0.0,q);
+      }
+
+      t.AddArc(prev_q,a);
       prev_q = q;
     }
     t.SetFinal(prev_q,0.0);
